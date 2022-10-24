08bfdfbc0dbc861de5d6699b0c7ee6547cc30cbf
Estefany Torres <35376654+estefanytorres@users.noreply.github.com>
2022-10-24 11:50:58 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,26 @@
+let fruit = document.querySelector(".fruit");
+let flower = document.querySelector(".flower");
+
+//Code Along
+
+// task 1: create an array called fruitList. Make sure it has
+//    at least 4 elements.
+
+// task 2: create a variable called favFruit. Assign the value
+//    equal to the array element of your favorite fruit in the
+//    fruitList array.
+
+// task 3: Uncomment the innerHTML of the fruit selector
+//    to see if the favFruit variable variable displays.
+// fruit.innerHTML = favFruit;
+
+let flowerList = ["cherry blossom", "sunflower", "hibiscus", "rose"];
+
+// task 4: remove the last element in the flowerList arrray.
+
+// task 5: add a new element to the flowerList arrray - pick
+//    a new flower not listed.
+
+// task 6: Uncomment the innerHTML of the flower selector
+//    to see if your array looks correct.
+// flower.innerHTML = flowerList.join(", ");
